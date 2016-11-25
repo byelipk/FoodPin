64b71444f40d8e454d75ffd9602b96207a75fbe5
byelipk <byelipk@gmail.com>
2016-11-25 17:00:21 -0500

    Animate the dialog

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var containerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,32 @@ class ReviewViewController: UIViewController {
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+        // The animation initial state
+        let scaleT = CGAffineTransform.init(scaleX: 0, y: 0)           // scale from 0
+        let slideT = CGAffineTransform.init(translationX: 0, y: -1000) // slide down from top
+        let combinedT = scaleT.concatenating(slideT)
+        containerView.transform = combinedT
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.3, animations: {() -> Void in
+            // CGAffineTransform.identity is a constant for resetting a view 
+            // to its original size and position
+            self.containerView.transform = CGAffineTransform.identity
+        })
+        
+        // Animate with spring
+//        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {() -> Void in
+//            self.containerView.transform = CGAffineTransform.identity
+//        }, completion: nil)
     }
     
 

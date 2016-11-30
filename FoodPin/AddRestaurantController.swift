8cd5e9992792d3f5ea3bc513efc344f0f14511f4
byelipk <byelipk@gmail.com>
2016-11-30 15:47:16 -0500

    Implement form validation when adding new restaurant

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -9,8 +9,16 @@
 import UIKit
 
 class AddRestaurantController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    
+    var isVisited = false
 
-    @IBOutlet var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -110,30 +118,65 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
         trailingConstraint.isActive   = true
         topConstraint.isActive        = true
         bottomConstraint.isActive     = true
-   
-        // Programatically set horizontal and vertical alignment for image
-        
-//        NSLayoutConstraint(
-//            item: photoImageView,
-//            attribute: NSLayoutAttribute.centerY,
-//            relatedBy: NSLayoutRelation.equal,
-//            toItem: photoImageView.superview,
-//            attribute: NSLayoutAttribute.centerY,
-//            multiplier: 1,
-//            constant: 0
-//            ).isActive = true
-//        
-//        NSLayoutConstraint(
-//            item: photoImageView,
-//            attribute: NSLayoutAttribute.centerX,
-//            relatedBy: NSLayoutRelation.equal,
-//            toItem: photoImageView.superview,
-//            attribute: NSLayoutAttribute.centerX,
-//            multiplier: 1,
-//            constant: 0
-//            ).isActive = true
         
         dismiss(animated: true, completion: nil)
     }
+    
+    func formIsValid() -> Bool {
+        if nameField.text?.isEmpty     == true ||
+           typeField.text?.isEmpty     == true ||
+           locationField.text?.isEmpty == true
+        {
+            return false
+        }
+        
+        return true
+    }
+    
+    func displayAlertToUser() -> Void {
+        let alertController = UIAlertController(
+            title: "Oh, shit!",
+            message: "There are some errors you need to fix before we can allow you to submit this form.",
+            preferredStyle: .alert
+        )
+        
+        let action1 = UIAlertAction(
+            title: "I get it",
+            style: .default,
+            handler: nil
+        )
+        
+        alertController.addAction(action1)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func handleValidForm() -> Void {
+        print("Nice job!")
+    }
+    
 
+    // ACTIONS
+    
+    @IBAction func saveRestaurant(_ sender: Any) {
+        if formIsValid() == true {
+            handleValidForm()
+        } else {
+            displayAlertToUser()
+        }
+    }
+    
+    @IBAction func toggleIsVisited(_ sender: UIButton) {
+        if sender == yesButton {
+            isVisited = true
+            yesButton.backgroundColor = UIColor.red
+            noButton.backgroundColor  = UIColor.lightGray
+        }
+            
+        else {
+            isVisited = false
+            yesButton.backgroundColor = UIColor.lightGray
+            noButton.backgroundColor  = UIColor.red
+        }
+    }
 }
